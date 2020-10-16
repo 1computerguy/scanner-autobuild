bf3ebcb21b33d5d9cb523b45f613802efdd240a8
1computerguy <31444089+1computerguy@users.noreply.github.com>
2020-10-16 13:56:16 -0400

    Reorganized file structure to support portability and function

---
+++ packer/setup-scripts/build-environment.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo '> Pulling scanner-autobuild git repo and submodules...'
+# download dod-compliance-and-automation git repository to build containers
+git clone https://github.com/mitre/heimdall.git
+git clone https://github.com/1computerguy/dod-compliance-and-automation.git
+
+echo '> Building and configuring heimdall visualization...'
+pushd heimdall
+./setup-docker-secrets.sh
+docker-compose up -d
+docker-compose run --rm web rake db:create db:migrate
+popd
+
+echo '> Building and configuring dod scanner...'
+docker-compose build --force-rm
+
+echo '> Making scan and remediate scripts executable...'
+# Setup scan and remediate automation scripts
+chmod +x /usr/local/bin/{scan,remediate}
+
+echo '> Removing initial build containers (no longer required)...'
+# Remove build containers
+docker rmi {chef/inspec,alpine:3.11}
