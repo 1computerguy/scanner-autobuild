32ff86bb990ba29d4f0ddb98dcbf4af04ce68633
1computerguy <31444089+1computerguy@users.noreply.github.com>
2020-10-28 14:31:59 -0400

    Added remedaite.sh - NOT started yet

---
+++ packer/environment/remediate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This script is not completed yet..."
+echo "If you'd like to contribute, check out the github repo below and submit a PR!"
+echo ""
+echo "https://github.com/1computerguy/scanner-autobuild"
+echo ""
