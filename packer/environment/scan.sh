7db14a670202465d70f97a723c67de75639394e5
1computerguy <31444089+1computerguy@users.noreply.github.com>
2020-10-29 12:33:54 -0400

    Updated help and added upload-user/api options

--- packer/environment/scan.sh
+++ packer/environment/scan.sh
@@ -9,74 +9,68 @@ RUN_DATE=$(date +"%m%d%Y")
 
 function help () {
     cat <<EOF
-  |-------------------------------------------------------------------------------------
-  |  This script performs STIG scans against VMware 6.7
-  |  environments ONLY. To use this script please indicate
-  |  what to scan.
+  |-------------------------------------------------------------------------------------------
+  |  This script performs STIG scans against VMware 6.7 environments ONLY.
+  |  To use this script and the associated DoD STIG validation scripts, refer to the
+  |  help information below.
+  |
+  | If you have problems, please contact Bryan Scarbrough
+  |   bscarbrough@vmware.com
+  |
+  |
+  | NOTE: Easiest usage is to modify the variables in the ~/.env file and point the script
+  |       to that file. See examples at the bottom of this message.
   |
   | Scan Arguments:
   |  --environment | -e <filename>      -  Scan based on variables set in <filename>
   |                                        See or edit the ~/.env file (or make your own)
+  |  --all | -a                         -  Scan all systems (VMs, ESX Hosts, and vCenter)
   |  --vm <all|VM name in vCenter>      -  Scan all or single VMs
   |  --host <all|Host FQDN or IP>       -  Scan all or single hosts
   |  --vcenter <vcenter FQDN or IP>     -  Scan vCenter VM
   |  --pip                              -  vCenter Photon IP address if scanning VCSA
-  |  --all | -a                         -  Scan all systems
   |  --upload | -u                      -  Upload to Heimdall
+  |  --upload-user <heimdal username>   -  Account for Heimdall visualization uploads
+  |                                        Value ignored if -u|--upload option not used
+  |  --upload-api-key <heimdall api key>-  API Key to use for Heimdall automatic uploads
+  |                                        Value ignored if -u|--upload option not used
   |  --control | -c <Control number>    -  Scan single control can be
   |                                        used with single host/vm or
   |                                        all VMs/Hosts
-  |  --creds | -d /credential/file      -  Use a file with credentials stored
-  |                                        in a base64 encoded user:pass format
   |  --creds | -d prompt                -  This option is to prompt for credentials
   |  --basedir | -b <script directory>  -  Set script resource directory
-  |  --force | -f                       -  Force rescan of system
+  |  --force | -f                       -  Force rescan of system (scanner ignores systems
+  |                                        scanned during the same day)
   |  --syslog | -s <syslog IP>          -  Dynamically set SysLog IP
-  |  --ntp1 | -n <NTP IP>               -  Set NTP1 IP
+  |  --ntp1 <NTP IP>                    -  Set NTP1 IP
   |  --ntp2 <NTP IP>                    -  Set NTP2 IP
-  |  --ntp | -n <NTP IP>                -  If -n used, first value is
-  |                                        first value is NTP1 and
-  |                                        second value is NTP2
+  |  --ntp | -n <NTP IP>                -  If -n used, first value is is NTP1 second
+  |                                        value is NTP2
   |
   | Usage of -n option:
-  |   $0 --vcenter --vc vcenter.domain -n '1.2.3.4' -n '2.3.4.5'
+  |   scan --vcenter --vc vcenter.some.domain -n '1.2.3.4' -n '2.3.4.5'
   |
-  |  --help                             -  Print this menu
+  |  --vcuser <username@some.domain> -  Set vCenter Username
+  |  --vcpass <vcenter password>     -  Set vCenter Password
+  |  --sshuser <ssh username>           -  Set vCenter Photon appliance SSH Username
+  |  --sshpass <ssh password>           -  Set vCenter Photon appliance SSH Password
   |
-  | NOTE: The below options are not recommended for security reasons
-  |     because your username and passwords will be stored in your
-  |     shell history. It is recommended to either:
-  |        1) Use the --creds prompt option and enter them when prompted. (most secure)
-  |        2) Use a file to store the credentials then use the
-  |           '--creds /file/location' option
-  |     creds file format:
-  |        - Base64 encode username and password as:
-  |                 username:password
-  |        - Example command to do this:
-  |           $ echo 'vcuser@domain:vcpassword' | base64 > .creds
-  |           $ echo 'sshuser:sshpassword' | base64 >> .creds
-  |
-  |  --user | -u <username@domain>      -  Set vCenter Username
-  |  --pass | -p <vcenter password>     -  Set vCenter Password
-  |  --sshuser <ssh username>           -  Set vCenter SSH Username
-  |  --sshpass <ssh password>           -  Set vCenter SSH Password
+  |  --help | -h                        -  Print this menu
   |
   | Example:
   |  Scan using .env file (easiest method)
-  |   $0 -e .env
+  |   scan -e .env
   |
   |  Scan all VMs and vCenter (vcenter has separate STIG)
-  |   $0 --vm all --vcenter --force --vc vcenter.domain
+  |   scan --vm all --vcenter --force --vc vcenter.some.domain
   |
   |  Scan single host with stored creds
-  |   $0 --host esxi1.domain --creds --vc vcenter.domain
+  |   scan --host esxi1.domain --creds --vc vcenter.some.domain
   |
   |  Scan all systems and prompt for creds (simplest but longest)
-  |   $0 --all --creds prompt --vc vcenter.domain
+  |   scan --all --creds prompt --vc vcenter.some.domain
   |
-  | If you have problems, please contact Bryan Scarbrough
-  |   bscarbrough@vmware.com
-  |__________________________________________________________________________________
+  |___________________________________________________________________________________________
 
 EOF
     exit 0
@@ -172,6 +166,14 @@ do
         --upload | -u )
             UPLOAD="1"
             ;;
+        --upload-user )
+            shift
+            UPLOAD_USER="$1"
+            ;;
+        --upload-api-key )
+            shift
+            UPLOAD_API_KEY="$1"
+            ;;
         --control | -c )
             shift
             CSCAN="$1"
@@ -218,11 +220,11 @@ do
             shift
             PASS="$1"
             ;;
-        --sshu )
+        --sshuser )
             shift
             SSH_USER="$1"
             ;;
-        --sshp )
+        --sshpass )
             shift
             SSH_PASS="$1"
             ;;
